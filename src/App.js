e055ba3978b9b7b3681ccd9918143b81440f1186
wijaya014 <wijaya.agus014@gmail.com>
2021-08-02 07:19:48 +0700

    reminder pro setup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Form, Button } from "react-bootstrap";
+export default function App() {
+  return (
+    <div>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </div>
+  );
+}
